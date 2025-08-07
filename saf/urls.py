7dd358dc133d5d0a83529377547bc6c12b69ae18
AS Abel Flecha <abelflecha@gmail.com>
2025-08-07 15:54:45 -0300

    Configuro URL raiz para la app articulos

--- saf/urls.py
+++ saf/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('articulos.urls')),  # Esto enlaza las URLs de tu app principal
+    path('', include('articulos.urls')),  # Asegúrate de que esta línea esté
 ]
+
 
