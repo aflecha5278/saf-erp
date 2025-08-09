7203bd7ea6966ab15bc3151918622b4165c9a6da
AS Abel Flecha <abelflecha@gmail.com>
2025-08-08 23:20:56 -0300

    Agrego whitenoise y actualizo requirements

--- saf/settings.py
+++ saf/settings.py
@@ -119,7 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -130,6 +129,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# al final de settings.py, agrega:
-STATIC_URL = '/static/'
 
